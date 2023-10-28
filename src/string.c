eacd9da096a4ae56383198259de3c1316bbf533d
Yurii <yurkakharitonyuk@gmail.com>
2023-10-28 17:52:57 +0300

    Added string

---
+++ src/string.c
@@ -0,0 +1,48 @@
+//
+// Created by Yurii on 10/28/2023.
+//
+
+#include "string.h"
+#include <string.h>
+#include <stdlib.h>
+
+String string_new(const char* string) {
+    String result;
+
+    result.length = strlen(string);
+
+    result.buffer = (char*)malloc(result.length);
+
+    strcpy(result.buffer, string);
+
+    return result;
+}
+
+String string_concatenate(const String* left, const String* right) {
+    String result;
+
+    result.length = left->length + right->length;
+
+    result.buffer = (char*)malloc(result.length);
+
+    strcpy(result.buffer, left->buffer);
+
+    strcat(result.buffer, right->buffer);
+
+    return result;
+}
+
+String string_copy(const String* string) {
+    String result;
+
+    result.length = string->length;
+    result.buffer = (char*)malloc(result.length);
+
+    strcpy(result.buffer, string->buffer);
+
+    return result;
+}
+
+void string_free(const String* string) {
+    free(string->buffer);
+}
---
+++ src/string.h
@@ -0,0 +1,23 @@
+//
+// Created by Yurii on 10/28/2023.
+//
+
+#ifndef CROSS_SOCKETS_STRING_H
+#define CROSS_SOCKETS_STRING_H
+
+#include <stdint.h>
+
+typedef struct {
+    char* buffer;
+    uint64_t length;
+} String;
+
+String string_new(const char* string);
+
+String string_concatenate(const String* left, const String* right);
+
+String string_copy(const String* string);
+
+void string_free(const String* string);
+
+#endif //CROSS_SOCKETS_STRING_H
